cd4c4a9bcbc3027a3d9a5e9b3ace659acad0a222
Choi Jaeho <31762986+kumoh2@users.noreply.github.com>
2025-04-23 06:38:04 +0900

    10171_Cat

---
+++ src/java/10171_Cat/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String Cat = "\\    /\\\n" +
+                        " )  ( ')\n" +
+                        "(  /  )\n" + 
+                        " \\(__)|";
+        System.out.println(Cat);
+    }
+}
