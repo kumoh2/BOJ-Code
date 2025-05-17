60b5b9232fc54429f92bd267aff2ada77eb48115
Choi Jaeho <31762986+kumoh2@users.noreply.github.com>
2025-05-17 10:43:55 +0900

    임시커밋

---
+++ src/java/3017_CloseNum/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        String sprout =
+       "         ,r'\"7\n" +
+       "r`-_   ,'  ,/\n" +
+       " \\. \". L_r'\n" +
+       "   `~\\/\n" +
+       "      |\n" +
+       "      |\n" ;
+
+       
+
+       System.out.println(sprout);
+    }
+}
+
+//          ,r'"7
+// r`-_   ,'  ,/
+//  \. ". L_r'
+//    `~\/
+//       |
+//       |
+
+//          ,r'"7
+// r`-_   ,'  ,/
+//  \. ". L_r'
+//    `~\/
+//       |
+//       |
