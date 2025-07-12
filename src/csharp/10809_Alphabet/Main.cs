c30a2998429dc28c4653bf2f67b0dd4526f1208c
Choi Jaeho <31762986+kumoh2@users.noreply.github.com>
2025-07-12 16:05:31 +0900

    C#_10809_Alphabet

---
+++ src/csharp/10809_Alphabet/Main.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program {
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine().Trim();
+        Console.WriteLine(FindFirstAtoZ(s));
+    }
+
+    /* 알파벳 A ~ Z 중 처음 등장하는 위치 */
+    static string FindFirstAtoZ(string s)
+    {
+        var cnt = new int[26]; // A ~ Z 의 첫번째 등장 위치 저장 배열
+
+        for (int i = 0; i < cnt.Length; i++)
+            cnt[i] = -1; // cnt 배열 초기화
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (cnt[s[i] - 'a'] == -1)
+            {
+                cnt[s[i] - 'a'] = i; // 해당 알파벳의 첫번째 등장 위치 저장
+            }          
+        }
+
+        return string.Join(" ", cnt);
+    }
+}
