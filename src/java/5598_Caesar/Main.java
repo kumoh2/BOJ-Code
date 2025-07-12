77ddadd3f6374d1294f236bd8c345ca09c299bc4
Choi Jaeho <31762986+kumoh2@users.noreply.github.com>
2025-07-12 17:28:48 +0900

    5598_Caesar

--- src/java/5598_Caesar/Main.java
+++ src/java/5598_Caesar/Main.java
@@ -1,29 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String sprout =
-       "         ,r'\"7\n" +
-       "r`-_   ,'  ,/\n" +
-       " \\. \". L_r'\n" +
-       "   `~\\/\n" +
-       "      |\n" +
-       "      |\n" ;
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        char[] encrypted = CaesarDecrypt(input);
+        System.out.println(new String(encrypted));
+        scanner.close();
+    }
 
-       
+    static char[] CaesarDecrypt(String s)
+    {
+        char[] decryptString = new char[s.length()];
 
-       System.out.println(sprout);
+        for (int i = 0; i < s.length(); i++)
+        {
+            decryptString[i] = (char)(s.charAt(i) - 3);
+
+            if( decryptString[i] < 'A' )
+            {
+                decryptString[i] = (char)(decryptString[i] + 26);
+            }
+        }
+
+        return decryptString;
     }
 }
-
-//          ,r'"7
-// r`-_   ,'  ,/
-//  \. ". L_r'
-//    `~\/
-//       |
-//       |
-
-//          ,r'"7
-// r`-_   ,'  ,/
-//  \. ". L_r'
-//    `~\/
-//       |
-//       |
